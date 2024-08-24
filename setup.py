edc9212e5b1eea7866edb54e546b97d9a592a05e
city-bridge <hironori0130@gmail.com>
2024-08-25 00:09:27 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='shared_json_server',
+    version='2024.8.16',
+    description='shared json server',
+    url='https://github.com/city-bridge/shared_json_server',
+    packages=[''],
+    install_requires=[
+        # List your dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
